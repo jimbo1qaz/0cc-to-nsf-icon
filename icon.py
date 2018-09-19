d35128294f61091575cbe1ddee9be7e92cbbd386
jimbo1qaz <jimbo1qaz@gmail.com>
2018-09-18 18:11:58 -0700

    Set linewidth = 0 by default

--- icon.py
+++ icon.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib as mpl
-mpl.rcParams['lines.linewidth'] = 0 # doesn't work
-
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import matplotlib.patches as patches
+
+mpl.rcParams['patch.linewidth'] = 0  # doesn't work
 
 # Use LaTeX throughout the figure for consistency
 # rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size': 16})
@@ -12,37 +12,38 @@ import matplotlib.patches as patches
 
 # Set up the figure.
 dpi = 480
-fig, ax1 = plt.subplots(1, 1, figsize=(512/dpi, 512/dpi), dpi=dpi,
-            gridspec_kw=dict(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
-    )
+fig, ax1 = plt.subplots(
+    1, 1, figsize=(512 / dpi, 512 / dpi), dpi=dpi,
+    gridspec_kw=dict(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+)
 
 SIZE = 1.1
+
 
 def setup():
     ax1.set_xlim(-SIZE, SIZE)
     ax1.set_ylim(-SIZE, SIZE)
     ax1.set_axis_off()
 
+
 # The parameter t in the parametric description of the superellipse
-t = np.linspace(0, 2*np.pi, 500)
+t = np.linspace(0, 2 * np.pi, 500)
 
 # Build an array of values of p up to pmax and assign the corresponding colours
-
-
 
 
 # exponent, width, height
 p = 4
 w = 1
 h = 0.9
-linewidth=1
+linewidth = 1
 color = 'k'
 bottom = '#282828'
 top = '#383838'
 
-text='0CC'
+text = '0CC'
 
-fcolor='w'
+fcolor = 'w'
 font = 'clear sans'
 fy = -6
 fsize = 34
@@ -56,8 +57,9 @@ hicolor = '#606060'
 shadow = False
 shadow_color = 'k'
 
-
 tnames = ['', '-transparent']
+
+
 def main():
     for transparent, suffix in enumerate(tnames):
         ax1.cla()
@@ -66,20 +68,21 @@ def main():
         if not transparent:
             kwargs = {'alpha': 1}
             c, s = np.cos(t), np.sin(t)
-            x = np.abs(c)**(2/p) * np.sign(c) * w
-            y = np.abs(s)**(2/p) * np.sign(s) * h
+            x = np.abs(c) ** (2 / p) * np.sign(c) * w
+            y = np.abs(s) ** (2 / p) * np.sign(s) * h
 
             if hi:
-                ax1.fill(x, y+hi, c=hicolor, **kwargs, linewidth=0)
+                ax1.fill(x, y + hi, c=hicolor, **kwargs)
             if shadow:
-                ax1.fill(x, y+shadow, c=shadow_color, **kwargs, linewidth=0)
+                ax1.fill(x, y + shadow, c=shadow_color, **kwargs)
             gradient_fill(x, y, bottom=bottom, top=top,
-                ax=ax1)
+                          ax=ax1)
             # ax1.plot(x, y, c=color, **kwargs, linewidth=linewidth)
 
         # Draw text
-        ax1.text(0,
-            fy/100,
+        ax1.text(
+            0,
+            fy / 100,
             text,
 
             family=font,
@@ -107,8 +110,8 @@ def gradient_fill(x, y, top, bottom, ax=None, zfunc=None):
         h, w = 100, 1
         z = np.empty((h, w, 4), dtype=float)
         rgb = mcolors.colorConverter.to_rgb(top)
-        z[:,:,:3] = rgb
-        z[:,:,-1] = np.linspace(0, alpha, h)[:,None]
+        z[:, :, :3] = rgb
+        z[:, :, -1] = np.linspace(0, alpha, h)[:, None]
     else:
         z = zfunc(x, y, fill_color=top, alpha=alpha)
     xmin, xmax, ymin, ymax = x.min(), x.max(), y.min(), y.max()
@@ -116,16 +119,16 @@ def gradient_fill(x, y, top, bottom, ax=None, zfunc=None):
     clippy = []
 
     # Background fill
-    bg = ax.fill(x, y, c=bottom, zorder=zorder, linewidth=0)
-    # clippy.extend(bg)
+    ax.fill(x, y, c=bottom, zorder=zorder)
 
     # Gradient fill
     im = ax.imshow(z, aspect='auto', extent=[xmin, xmax, ymin, ymax],
-                   origin='lower', zorder=zorder+1)
+                   origin='lower', zorder=zorder + 1)
     clippy.append(im)
 
     xy = np.column_stack([x, y])
-    clip_path = patches.Polygon(xy, facecolor='none', edgecolor='none', closed=True, linewidth=0)
+    clip_path = patches.Polygon(xy, facecolor='none', edgecolor='none', closed=True,
+                                linewidth=0)
     ax.add_patch(clip_path)
 
     for clip in clippy:
